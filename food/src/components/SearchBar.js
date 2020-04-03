4dd3604025ad88322a41493e91d7c345eb8d233c
Cahit Yusuf KAFADAR <cahityusuf@gmail.com>
2020-04-03 23:23:38 +0300

    proje devam

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {TextInput,StyleSheet,View} from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar=()=>{
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" style={styles.iconStyle}/>
+            <TextInput
+            placeholder="Search"
+            style={styles.inputStyle}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle:{
+        marginTop:10,
+        backgroundColor:'#F0EEEE',
+        height:50,
+        borderRadius:5,
+        marginHorizontal:15,
+        flexDirection:"row",
+        
+    },
+    inputStyle:{
+        fontSize:18,
+        flex:1
+    },
+    iconStyle:{
+        fontSize:35,
+        alignSelf:"center",
+        marginHorizontal:15
+    }
+});
+
+export default SearchBar;
